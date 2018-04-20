b5e4234a2d9b8170e6f5ee23e0a97f4f727e85b2
Paul A Hansen <emperorxlii@hotmail.com>
2018-04-20 15:09:41 -0500

    This flex layout is no good :/  Moving on!

--- fluidifyLayout.js
+++ fluidifyLayout.js
@@ -1,14 +1,35 @@
 function promoteChildren() {
   var self = $(this)
-  self.children().prependTo(self.parent())
+  self.children().prependTo( self.parent() )
   self.remove()
 }
 
 (function run() {
+  // Enable side-by-side display of small fields.
+  var largeFields = [
+    '.col_1_1',
+    '.col_2_2',
+    '.col_3_3'
+  ];
+  var smallFields = [
+    '.col_2_1',
+    '.col_3_1',
+    '.col_3_2'
+  ];
+  largeFields.forEach( fieldClass => $(fieldClass).children().addClass( 'large-field' ) )
+  smallFields.forEach( fieldClass => $(fieldClass).children().addClass( 'small-field' ) )
+  
   // No rows! No columns!
-  ['.row','.col_1_1','.col_2_1','.col_2_2','.col_3_1','.col_3_2','.col_3_3'].forEach(
-    carClass => $(carClass).each( promoteChildren )
-  )
+  var allFields = [
+    '.row',
+    '.col_1_1',
+    '.col_2_1',
+    '.col_2_2',
+    '.col_3_1',
+    '.col_3_2',
+    '.col_3_3'
+  ];
+  allFields.forEach( fieldClass => $(fieldClass).each( promoteChildren ) )
   
   // Suppress sidebars.
   $('.sectionsTopBar').remove()
