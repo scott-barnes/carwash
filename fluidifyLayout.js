7b3901adc697e02f9e808300c80a47a0f5a0bd4c
Paul A Hansen <emperorxlii@hotmail.com>
2018-04-20 13:18:30 -0500

    More visual simplicitification.

--- fluidifyLayout.js
+++ fluidifyLayout.js
@@ -1,10 +1,20 @@
+function promoteChildren() {
+  var self = $(this)
+  self.children().prependTo(self.parent())
+  self.remove()
+}
+
 (function run() {
   // No rows! No columns!
-  ['.row','.col_1_1','.col_2_1','.col_2_2','.col_3_1','.col_3_2','.col_3_3'].forEach(carClass => {
-    $(carClass).each(function() {
-      var self = $(this)
-      self.children().prependTo(self.parent())
-      self.remove()
-    })
-  })
+  ['.row','.col_1_1','.col_2_1','.col_2_2','.col_3_1','.col_3_2','.col_3_3'].forEach(
+    carClass => $(carClass).each( promoteChildren )
+  )
+  
+  // Suppress sidebars.
+  $('.sectionsTopBar').remove()
+  $('#sidebarInner').remove()
+  $('#sectionsInner').each( promoteChildren )
+  
+  // Eliminate shadows.
+  $('.section-content-shadow').remove()
 })();
