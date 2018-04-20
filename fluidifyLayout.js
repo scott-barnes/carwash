552f773338fa57b8722513704be7599102fc133e
Paul A Hansen <emperorxlii@hotmail.com>
2018-04-20 12:08:58 -0500

    Removed columns.

--- fluidifyLayout.js
+++ fluidifyLayout.js
@@ -1,3 +1,10 @@
 (function run() {
-  $('.labelContainer').css({ 'background-color': '#82AED1' })
+  // No columns!
+  ['.col_1_1','.col_2_1','.col_2_2','.col_3_1','.col_3_2','.col_3_3'].forEach(carClass => {
+    $(carClass).each(function() {
+      var self = $(this)
+      self.children().appendTo(self.parent())
+      self.remove()
+    })
+  })
 })();
