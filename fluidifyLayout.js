cc035fd81e21db7adc33eb0e8581966ec2f5b789
Paul A Hansen <emperorxlii@hotmail.com>
2018-04-20 12:26:57 -0500

    Removed rows, and added field style extension.

--- fluidifyLayout.js
+++ fluidifyLayout.js
@@ -1,9 +1,9 @@
 (function run() {
-  // No columns!
-  ['.col_1_1','.col_2_1','.col_2_2','.col_3_1','.col_3_2','.col_3_3'].forEach(carClass => {
+  // No rows! No columns!
+  ['.row','.col_1_1','.col_2_1','.col_2_2','.col_3_1','.col_3_2','.col_3_3'].forEach(carClass => {
     $(carClass).each(function() {
       var self = $(this)
-      self.children().appendTo(self.parent())
+      self.children().prependTo(self.parent())
       self.remove()
     })
   })
